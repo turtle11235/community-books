a5e9d756c64334065101cb0042a889f3cd648b6e
Emrys Shevek <mpoggemann96@gmail.com>
2023-12-06 13:13:08 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import Test from './components/test/Test';
+import './App.css';
+
+function App() {
+  return (
+    <Test></Test>
+  );
+}
+
+export default App;
